d7381059d7c7fa4725c1f425c0e340ce803826df
Bittupandey5288 <kumarbittu2720@gmail.com>
2026-04-17 00:53:55 +0530

    V1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('example file', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
+//
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 })
